977299b730a2ff4f9f5ee5015d5bd41b1b2ef62c
Mia Chen <mia89926@gmail.com>
2020-06-01 11:12:32 +0800

    feat: add existed url checking function

--- app.js
+++ app.js
@@ -21,36 +21,43 @@ app.get('/', (req, res) => {
 // 輸入之後製造短網址，建立一筆資料，渲染結果到頁面上
 app.post('/', (req, res) => {
   const inputURL = req.body.inputURL
-  
-  
   if (inputURL === '') { // 空白輸入
     let errMessage = "Please enter the URL!!"
     res.render('index', { errMessage, inputURL })
     return
   } else if (inputURL.includes("https://") || inputURL.includes("http://")){
-    let shortenedURL = generateURL() 
-    return Url.find()
-    .lean()
-    .then((url) => {
-      url.forEach(u => {
-        // 防止重複短網址出現
-        while( u.shortenedURL.includes( shortenedURL )) {
-          shortenedURL = generateURL()
+    // 輸入URL符合格式
+    // 檢查是否已存在其短網址
+    Url.findOne({ originURL: inputURL })
+      .lean()
+      .then(existURL => {
+        if (existURL) {
+          let errMessage = `Short URL existed: ${existURL.shortenedURL}`
+          return res.render('index', { errMessage, inputURL })
+        } else {
+          // 沒有則新增一筆
+          let shortenedURL = generateURL() 
+          // 防止重複短網址出現
+          Url.find()
+            .lean()
+            .then((url) => {
+              url.forEach(u => {
+                while( u.shortenedURL.includes( shortenedURL )) {
+                  shortenedURL = generateURL()
+                }
+               return shortenedURL
+              })
+            })
+            .then(() => Url.create({ originURL:inputURL, shortenedURL }))
+            .then(() => res.render('show', { newURL: shortenedURL }))
+            .catch(error => console.log(error))    
         }
-      return shortenedURL
-      })
-    })
-    .then(() => Url.create({ originURL:inputURL, shortenedURL }))
-    .then(() => res.render('show', { newURL: shortenedURL }))
-    .catch(error => console.log(error))     
+      })    
   } else { // 不符的輸入
-    let errMessage = "This is not an URL! Please enter again </br> ( https://... OR http://... )"
+    let errMessage = "This is not an URL! Please enter again."
     res.render('index', { errMessage, inputURL })
     return
   }
-
-  
-    
 })
 
 // 在伺服器啟動期間，使用者輸入短網址可進入原網址頁面
